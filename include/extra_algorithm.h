db68e383c38389f1d16a6936a38012b7f8ed1ee5
Tihran Katolikian <moreggmoreskill@icloud.com>
2019-11-07 21:06:20 +0200

    Fix algorithms

--- include/extra_algorithm.h
+++ include/extra_algorithm.h
@@ -8,14 +8,14 @@ namespace estd
 {
 
 template <class TContainer, class TValue>
-inline bool contains(TContainer const& container, TValue const& value)
+inline bool contains(const TContainer& container, const TValue& value)
 {
-    auto const cend = container.cend();
+    const auto cend = container.cend();
 
     if constexpr (has_key_type_v<TContainer> && has_mapped_type_v<TContainer> &&
         has_method_find_v<TContainer, TValue>)
     {
-        return container.find(static_cast<typename TContainer::key_type const&>(value)) != cend;
+        return container.find(static_cast<const typename TContainer::key_type&>(value)) != cend;
     }
     else
     {
@@ -24,102 +24,102 @@ inline bool contains(TContainer const& container, TValue const& value)
 }
 
 template <class TContainer, class TPred>
-inline bool contains_if(TContainer const& container, TPred const& pred)
+inline bool contains_if(const TContainer& container, TPred&& pred)
 {
-    auto const cend = container.cend();
-    return std::find_if(container.cbegin(), cend, pred) != cend;
+    const auto cend = container.cend();
+    return std::find_if(container.cbegin(), cend, std::forward<TPred>(pred)) != cend;
 }
 
 template <class TContainer, class TValue>
-inline std::pair<bool, typename TContainer::const_iterator> find(TContainer const& container, TValue const& value)
+inline std::pair<bool, typename TContainer::const_iterator> find(const TContainer& container, const TValue& value)
 {
-    auto const cend = container.cend();
+    const auto cend = container.cend();
 
     if constexpr (has_key_type_v<TContainer> && has_mapped_type_v<TContainer> &&
         has_method_find_v<TContainer, TValue>)
     {
-        auto const it = container.find(value);
+        const auto it = container.find(value);
         return { it != cend, it };
     }
     else
     {
-        auto const it = std::find(container.cbegin(), cend, value);
+        const auto it = std::find(container.cbegin(), cend, value);
         return { it != cend, it };
     }
 }
 
 template <class TContainer, class TValue>
-inline std::pair<bool, typename TContainer::iterator> find(TContainer& container, TValue const& value)
+inline std::pair<bool, typename TContainer::iterator> find(TContainer& container, const TValue& value)
 {
-    auto const end = container.end();
+    const auto end = container.end();
 
     if constexpr (has_key_type_v<TContainer> && has_mapped_type_v<TContainer> &&
-        has_method_find_v<TContainer, typename TContainer::key_type>)
+        has_method_find_v<TContainer, TValue>)
     {
-        auto const it = container.find(value);
+        const auto it = container.find(value);
         return { it != end, it };
     }
     else
     {
-        auto const it = std::find(container.begin(), end, value);
+        const auto it = std::find(container.begin(), end, value);
         return { it != end, it };
     }
 }
 
 template <class TContainer, class TPred>
-inline std::pair<bool, typename TContainer::const_iterator> find_if(TContainer const& container, TPred const& pred)
+inline std::pair<bool, typename TContainer::const_iterator> find_if(const TContainer& container, TPred&& pred)
 {
     auto const cend = container.cend();
-    auto const it = std::find_if(container.cbegin(), cend, pred);
+    auto const it = std::find_if(container.cbegin(), cend, std::forward<TPred>(pred));
     return { it != cend, it };
 }
 
 template <class TContainer, class TPred>
-inline std::pair<bool, typename TContainer::iterator> find_if(TContainer& container, TPred const& pred)
+inline std::pair<bool, typename TContainer::iterator> find_if(TContainer& container, TPred&& pred)
 {
     auto const end = container.end();
-    auto const it = std::find_if(container.begin(), end, pred);
+    auto const it = std::find_if(container.begin(), end, std::forward<TPred>(pred));
     return { it != end, it };
 }
 
 template <class TContainer, class TPred>
-inline bool all(TContainer const& container, TPred const& pred)
+inline bool all(const TContainer& container, TPred&& pred)
 {
-    return std::all_of(container.cbegin(), container.cend(), pred);
+    return std::all_of(container.cbegin(), container.cend(), std::forward<TPred>(pred));
 }
 
 template <class TContainer>
 inline bool are_unique(TContainer container)
 {
-    auto oldEnd = container.end();
-    auto it = std::unique(container.begin(), container.end());
+    const auto oldEnd = container.end();
+    const auto it = std::unique(container.begin(), container.end());
     return it == oldEnd;
 }
 
 template <class TContainer, class TContainerToAppend>
-inline void append(TContainer& base, TContainerToAppend const& toAppend)
+inline void append(TContainer& base, const TContainerToAppend& toAppend)
 {
     std::copy(toAppend.cbegin(), toAppend.cend(), std::back_inserter(base));
 }
 
 template <class TContainer, class TContainerToAppend, class TPred>
-inline void append_if(TContainer& base, TContainerToAppend const& toAppend, TPred const& pred)
+inline void append_if(TContainer& base, const TContainerToAppend& toAppend, TPred&& pred)
 {
-    std::copy_if(toAppend.cbegin(), toAppend.cend(), std::back_inserter(base), pred);
+    std::copy_if(toAppend.cbegin(), toAppend.cend(), std::back_inserter(base), std::forward<TPred>(pred));
 }
 
 template <class TContainer, class TPred>
-inline void remove_if(TContainer& container, TPred const& pred)
+inline void remove_if(TContainer& container, TPred&& pred)
 {
-    auto it = std::remove_if(container.begin(), container.end(), pred);
+    const auto it = std::remove_if(container.begin(), container.end(), std::forward<TPred>(pred));
     container.erase(it, container.cend());
 }
 
 template <class TContainer, class TFilter>
-TContainer filter(TContainer const& orig, TFilter const& filter)
+TContainer filter(TContainer const& orig, TFilter&& filter)
 {
     TContainer filtered;
-    std::copy_if(orig.cbegin(), orig.cend(), std::back_inserter(filtered), filter);
+    std::copy_if(orig.cbegin(), orig.cend(), std::back_inserter(filtered), std::forward<TFilter>(filter));
     return filtered;
 }
 
