1fe1d03b9ee5e564e7167c2ec7b2f7a16718e5a9
Tihran Katolikian <moreggmoreskill@icloud.com>
2020-04-24 08:25:53 +0300

    Fix extra_std.h

--- extra_std.h
+++ extra_std.h
@@ -3,7 +3,6 @@
 #include "include/compile_time_magic.h"
 #include "include/assert.h"
 #include "include/extra_algorithm.h"
-#include "include/extra_std.h"
 #include "include/has_method.h"
 #include "include/has_using.h"
 #include "include/string_utils.h"
