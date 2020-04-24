29e8dfe264bb91b7be1b78d5aab4ea8b3740a03d
Tihran Katolikian <moreggmoreskill@icloud.com>
2020-04-24 08:13:36 +0300

    Fix assert

--- include/assert.h
+++ include/assert.h
@@ -3,10 +3,6 @@
 #include <cassert>
 #include "string_utils.h"
 
-#ifndef ESTD_ASSERT_NAME
-#define ESTD_ASSERT_NAME ASSERT
-#endif
-
-#define ESTD_ASSERT_NAME(expr, fmt, ...)\
+#define ESTD_ASSERT(expr, fmt, ...)\
 assert(expr && estd::format(fmt, ...).c_str())\
 
